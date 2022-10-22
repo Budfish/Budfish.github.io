a23fc8bc4b0bde12b2c391222679a39757667444
Budfish <87319891+Budfish@users.noreply.github.com>
2022-10-22 16:12:16 +0800

    [ch2 binaryTree] create function window

--- knowledge/scripts/ch2_binaryTree.js
+++ knowledge/scripts/ch2_binaryTree.js
@@ -6,11 +6,20 @@ const stageHeight = 3;
 
 // variables
 const treeArea = $("#treeArea")[0];
+const bottomArea = $("#bottomArea")[0];
 const line = $(".line")[0];
 const halfGap = nodeGap / 1.6;
 const halfWidth = nodeWidth / 2;
 const rem = parseFloat($('body').css('font-size'));
 const globalShift = parseFloat($(treeArea).css("width")) / rem / 2;
+const code_GetHeight =
+    `function GetHeight(node){
+    if(!node) return 0;
+    let L_height = GetHeight(node.left);
+    let R_height = GetHeight(node.right);
+    let ans = max(L_height,R_height) + 1;
+    return ans;
+}`;
 let allNode = [];
 
 // classes
@@ -137,7 +146,122 @@ class TreeNodeFactory {
         return null;
     }
 }
+class CodeFactory {
+    constructor() {
+        this.fwid = "_";
+        this.getHeightFactory = new Code_GetHeightFactory();
+    }
+    RunGetHeight() {
+        this.fwid = "_";
+        this.ct = 0;
+        $(bottomArea).html("");
+        this.GetHeight(treeFactory.root, null);
+    }
+    GetHeight(node, parent, lr = 0) {
+        // lr: 0->l, 1->r;
+        this.CreateFunctionWindow(this.fwid);
+        if (!node) {
+            this.getHeightFactory.EarlyReturn(this.fwid, parent, lr);
+            return 0;
+        }
+
+        this.getHeightFactory.UpdateLine_0(this.fwid, node);
+        this.fwid += "l";
+        let L_height = this.GetHeight(node.left, node, 0);
+        this.Cutfwdid();
+
+        this.getHeightFactory.UpdateLine_1(this.fwid, node, L_height);
+        this.fwid += "r";
+        let R_height = this.GetHeight(node.right, node, 1);
+        this.Cutfwdid();
+
+        this.getHeightFactory.UpdateLine_2(this.fwid, L_height, R_height);
+        let ans = max(L_height, R_height) + 1;
+
+        this.getHeightFactory.UpdateLine_3(this.fwid, ans);
+        return ans;
+    }
+    CreateFunctionWindow(fwid) {
+        let fwStr = `<div id="fw${fwid}" style="top:${this.ct}rem" class="functionWindow"><div id="fta${fwid}" class="functionTextArea"></div></div>`;
+        this.ct += 12;
+
+        $(bottomArea).append(fwStr);
+    }
+    Cutfwdid() {
+        this.fwid = this.fwid.substring(0, this.fwid.length - 1);
+    }
+}
+class Code_GetHeightFactory {
+    constructor() {
+
+    }
+    EarlyReturn(fwid, parent, lr) {
+        let nodeReplace = `<span nodeid="${parent.id + (lr ? "r" : "l")}" class="variable node">node</span>`;
+        let str = `
+        <p>function GetHeight(node){</p>
+        <p> &nbsp&nbsp&nbsp&nbspif(!node) return 0;</p>`.replace(/\bnode\b/g, nodeReplace);
+        str += `
+        <p class="gray"> &nbsp&nbsp&nbsp&nbsplet L_height = GetHeight(node.left);</p>
+        <p class="gray"> &nbsp&nbsp&nbsp&nbsplet R_height = GetHeight(node.right);</p>
+        <p class="gray"> &nbsp&nbsp&nbsp&nbsplet ans = max(L_height,R_height)+1;</p>
+        <p class="gray"> &nbsp&nbsp&nbsp&nbspreturn ans;</p>
+        <p>}</p>`;
+        $(`#fta${fwid}`).append(str);
+    }
+    UpdateLine_0(fwid, node) {
+        let nodeReplace = `<span nodeid="${node.id}" class="variable node">node</span>`;
+        let leftReplace = `<span nodeid="${node.id}l" class="variable node">left</span>`;
+        let L_heightReplace = `<span id="lh0${fwid}" class="variable">L_height</span>`;
+        let str = `
+        <p>function GetHeight(node){</p>
+        <p> &nbsp&nbsp&nbsp&nbspif(!node) return 0;</p>
+        <p> &nbsp&nbsp&nbsp&nbsplet L_height = GetHeight(node.left);</p>`;
+        str = str.replace(/\bnode\b/g, nodeReplace);
+        str = str.replace(/\bleft\b/g, leftReplace);
+        str = str.replace(/\bL_height\b/g, L_heightReplace);
+        $(`#fta${fwid}`).append(str);
+    }
+    UpdateLine_1(fwid, node, L_height) {
+        $(`#lh0${fwid}`).attr("val", L_height);
+        let nodeReplace = `<span nodeid="${node.id}" class="variable node">node</span>`;
+        let rightReplace = `<span nodeid="${node.id}r" class="variable node">right</span>`;
+        let R_heightReplace = `<span id="rh0${fwid}" class="variable">R_height</span>`;
+        let str = `<p> &nbsp&nbsp&nbsp&nbsplet R_height = GetHeight(node.right);</p>`;
+        str = str.replace(/\bnode\b/g, nodeReplace);
+        str = str.replace(/\bright\b/g, rightReplace);
+        str = str.replace(/\bR_height\b/g, R_heightReplace);
+        $(`#fta${fwid}`).append(str);
+    }
+    UpdateLine_2(fwid, L_height, R_height) {
+        $(`#rh0${fwid}`).attr("val", R_height);
+        let ansReplace = `<span  val="${max(L_height + R_height) + 1}" class="variable node">ans</span>`;
+        let L_heightReplace = `<span val="${L_height}" class="variable node">L_height</span>`;
+        let R_heightReplace = `<span val="${R_height}" class="variable">R_height</span>`;
+        let str = `<p> &nbsp&nbsp&nbsp&nbsplet ans = max(L_height,R_height)+1;</p>`;
+        str = str.replace(/\bans\b/g, ansReplace);
+        str = str.replace(/\bL_height\b/g, L_heightReplace);
+        str = str.replace(/\bR_height\b/g, R_heightReplace);
+        $(`#fta${fwid}`).append(str);
+    }
+    UpdateLine_3(fwid, ans) {
+        let ansReplace = `<span  val="${ans}" class="variable node">ans</span>`;
+        let str = `<p> &nbsp&nbsp&nbsp&nbspreturn ans;</p><p>}</p>`;
+        str = str.replace(/\bans\b/g, ansReplace);
+        $(`#fta${fwid}`).append(str);
+    }
+}
+
+function max(a, b) {
+    if (a > b) return a;
+    return b;
+}
 
 // initialization
 let treeFactory = new TreeNodeFactory();
+let codeFactory = new CodeFactory();
 treeFactory.Init();
+
+$("#runCode").click((e) => {
+    codeFactory.RunGetHeight();
+})
+$("#userCode").val(code_GetHeight);
